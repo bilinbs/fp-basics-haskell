ccb8fa93aaa207b09a64a59ea8cfe944ab20b829
bilinbs <bilinbs@gmail.com>
2017-03-27 22:32:00 +0530

    added point data type, remove point in curve check before addition

--- code/number_theory/elliptic_curves.hs
+++ code/number_theory/elliptic_curves.hs
@@ -1,54 +1,53 @@
 import Data.Maybe
 
-addEllP::EllpCurve->(Integer,Integer)->(Integer,Integer)->Integer->
-            Maybe (Integer,Integer)
-addEllP (EllpCurve a b) (x1,y1) (x2,y2) p 
-                = if ((pointInCurve (EllpCurve a b) (x1,y1) p) &&
-                         (pointInCurve (EllpCurve a b) (x2,y2) p)) then
-                    let
-                        m = if ((x1==x2) && (y1==y2)) then
-                                modMult ((modMult 3 (modPow x1 2 p) p)+a) 
-                                    (inverse (modMult 2 y1 p) p) p
-                            else
-                                modMult (y2-y1) (inverse (x2-x1) p) p
-                        x3 = positiveConvMod ((modMult m m p) - x1 -x2) p
-                        y3 = positiveConvMod ((modMult m (positiveConvMod (x1-x3) p) p) - y1) p
-                    in
-                        Just (x3,y3)                                                    
-                  else Nothing
+addEllP::EllpCurve->Point->Point->Integer->Point
+addEllP curve point NullPoint _ = point
+addEllP curve NullPoint point _ = point 
+addEllP (EllpCurve a b) (Point x1 y1) (Point x2 y2) p 
+                | x1 == x2 && y1 == (p - y2) = NullPoint
+                | otherwise =   let
+                                    m = if ((x1==x2) && (y1==y2)) then
+                                            modMult ((modMult 3 (modPow x1 2 p) p)+a) 
+                                                (inverse (modMult 2 y1 p) p) p
+                                        else
+                                            modMult (y2-y1) (inverse (x2-x1) p) p
+                                    x3 = positiveConvMod ((modMult m m p) - x1 -x2) p
+                                    y3 = positiveConvMod ((modMult m (positiveConvMod (x1-x3) p) p) - y1) p
+                                in
+                                    (Point x3 y3)                                                    
+                  
 
-doubleEllp::EllpCurve->(Integer,Integer)->Integer->Maybe (Integer,Integer)
-doubleEllp e (x1, y1) p = addEllP e (x1,y1) (x1,y1) p
+doubleEllp::EllpCurve->Point->Integer->Point
+doubleEllp e point p = addEllP e point point p
 
-nthMultEllp::EllpCurve->(Integer,Integer)->Integer->Integer->
-                Maybe (Integer,Integer)
+nthMultEllp::EllpCurve->Point->Integer->Integer->Point
 nthMultEllp curve point n p = let 
-                                    p2 = fromJust (doubleEllp curve point p)
+                                    p2 = (doubleEllp curve point p)
                                 in 
                                     nthMultEllp' curve p2 point (n-1) p
-nthMultEllp' curve (accX,accY) (x1,y1) n p 
-                | n == 1 = Just (accX,accY)
+nthMultEllp' curve acc p1 n p 
+                | n == 1 = acc
                 | otherwise = let 
-                                (x2,y2) = 
-                                    fromJust (addEllP curve (accX,accY) (x1,y1) p)
+                                p2 = 
+                                    addEllP curve acc p1 p
                               in
-                                nthMultEllp' curve (x2,y2) (x1,y1) (n-1) p
+                                nthMultEllp' curve p2 p1 (n-1) p
 
 
 --input elliptic curve, two points p, q , integer field value m
 --output n such that n.p = q
-divEllp::EllpCurve->(Integer,Integer)->(Integer,Integer)->Integer->Integer
+divEllp::EllpCurve->Point->Point->Integer->Integer
 divEllp curve p q m = divEllp' curve p q m 2
 divEllp' curve p q m n = let 
-                            np = fromJust (nthMultEllp curve p n m)
+                            np = nthMultEllp curve p n m
                          in
                             if np == q then
                                 n
                             else
                                 divEllp' curve p q m (n+1) 
 
-pointInCurve::EllpCurve->(Integer,Integer)->Integer->Bool
-pointInCurve (EllpCurve a b) (x,y) p = 
+pointInCurve::EllpCurve->Point->Integer->Bool
+pointInCurve (EllpCurve a b) (Point x y) p = 
     (modPow y 2 p) == positiveConvMod ((modPow x 3 p) + 
         (modMult a x p) + (positiveConvMod b p)) p
 
@@ -74,4 +73,18 @@ positiveConvMod n m | n<0 = positiveConvMod (n+m) m
                     | n>=m = positiveConvMod (n-m) m
                     | otherwise = n
 
-data EllpCurve = EllpCurve Integer Integer 
+data EllpCurve = EllpCurve Integer Integer
+instance Show EllpCurve where
+    show (EllpCurve a b) = "y^2 = x^3 " ++ (signStr a) ++ 
+        (show (abs a)) ++ "x "++ (signStr b) ++ (show (abs b)) 
+data Point = Point Integer Integer | NullPoint
+instance Eq Point where
+    (==) (Point x1 y1) (Point x2 y2) = (x1 == x2) && (y1 == y2) 
+
+instance Show Point where
+    show (Point x y) = "(" ++ (show x) ++ ", " ++ (show y) ++ ")" 
+    show NullPoint = "O"
+
+signStr::Integer->String
+signStr x | x < 0 = "- "
+          | otherwise = "+ "
